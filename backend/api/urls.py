5116d185c77361710494f292ad5419378bec133a
Ambarsariya007 <mvsbasra2006@gmail.com>
2025-04-03 12:08:27 +0530

    removed"api"from urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RegisterView, LoginView, UserDetailView
 
 urlpatterns = [
-    path('api/register/', RegisterView.as_view(), name='api-register'),
-    path('api/login/', LoginView.as_view(), name='api-login'),
-    path('api/user/', UserDetailView.as_view(), name='api-user'),
+    path('register/', RegisterView.as_view(), name='api-register'),
+    path('login/', LoginView.as_view(), name='api-login'),
+    path('user/', UserDetailView.as_view(), name='api-user'),
 ]
