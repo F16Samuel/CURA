968d078e59160581e5266a893f83da358307c901
Ambarsariya007 <mvsbasra2006@gmail.com>
2025-04-03 11:50:17 +0530

    serialzers

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+class LoginView(TokenObtainPairView):
+    serializer_class = LoginSerializer
+
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
