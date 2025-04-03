0478bf9ab2ed41c5f8ada50b43a0dd04ec79ba47
Shauviq <shauviqmishra@gmail.com>
2025-04-03 20:18:58 +0530

    Providers Page Done

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -40,7 +40,7 @@ const Login = () => {
     try {
       const csrfToken = getCookie("csrftoken"); // Get CSRF token from cookies
 
-      const response = await fetch("http://127.0.0.1:8000/login/", {
+      const response = await fetch("http://localhost:8000/login/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- frontend/src/Routes/CustomRoutes.jsx
+++ frontend/src/Routes/CustomRoutes.jsx
@@ -3,6 +3,7 @@ import SignupPage from "../Components/Pages/Signup_page";
 import Home from "../Components/Pages/Home";
 import LoginPage from "../Components/Pages/Login_page"
 import AiPage from "../Components/Pages/AiPage";
+import Provider_page from "../Components/Pages/Providers_page";
 
 
 function CustomRoutes() {
@@ -13,6 +14,7 @@ function CustomRoutes() {
             <Route path="SignUp" element = {<SignupPage/>}/>
             <Route path="Login" element = {<LoginPage/>}/>
             <Route path="ai-consultation" element = {<AiPage/>}/>
+            <Route path="providers" element = {<Provider_page/>}/>
 
         </Routes>
     );
